e4c7c76f3db1212c52bab8be51d7ebb7d214dc49
kumavip <kumarvipul1513@gmail.com>
2024-09-17 22:17:49 +0530

    initial status

---
+++ src/test/java/com/vipul/learn_oauth/LearnOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vipul.learn_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
